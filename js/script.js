33dc08e48e866b5166a8e79a0d5afb0521bd9566
Overs <abogadoberruecos@gmail.com>
2024-08-21 18:49:23 -0500

    p1

---
+++ js/script.js
@@ -0,0 +1,2 @@
+let ProgrammingLanguage = prompt("Best programming language?");
+document.getElementById("language").textContent = ProgrammingLanguage;
---
+++ js/script2.js
@@ -0,0 +1,7 @@
+let FirstName = "Jorge";
+let LastName = "Berruecos";
+let programName = "Full Stack Web Development";
+
+document.getElementById("myname").textContent = FirstName + "" + LastName;
+document.getElementById("myname1").textContent = FirstName + "" + LastName;
+document.getElementById("programname").textContent = programName;
